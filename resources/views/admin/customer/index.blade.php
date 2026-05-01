dd813194748d9300037f3a5606dd40cd4893b67f
parthokar90 <parthokar90@gmail.com>
2026-05-01 22:32:44 +0600

    refactor: Simplify customer index view layout and remove DataTables

--- resources/views/admin/customer/index.blade.php
+++ resources/views/admin/customer/index.blade.php
@@ -3,72 +3,51 @@
 @section('title') Customers @endsection
 
 @section('content')
+
 <div class="container-fluid">
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header d-flex justify-content-between align-items-center">
-                    <h3 class="card-title">Customer List</h3>
-                    <a href="{{ route('customers.create') }}" class="btn btn-primary">
-                        <i class="fas fa-plus"></i> Add New Customer
-                    </a>
-                </div>
-                <div class="card-body">
-                    <table id="customers-table" class="table table-bordered table-striped">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Mobile</th>
-                                <th>Status</th>
-                                <th>Created At</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
+
+    <a href="{{ route('customers.create') }}" class="btn btn-primary mb-3">
+        Add Customer
+    </a>
+
+    <table class="table table-bordered">
+
+        <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Mobile</th>
+            <th>Status</th>
+            <th>Action</th>
+        </tr>
+
+        @foreach($customers as $key => $customer)
+        <tr>
+            <td>{{ $key+1 }}</td>
+            <td>{{ $customer->first_name }} {{ $customer->last_name }}</td>
+            <td>{{ $customer->email }}</td>
+            <td>{{ $customer->mobile }}</td>
+            <td>
+                @if($customer->status == 1)
+                <span class="badge badge-success">Active</span>
+                @else
+                <span class="badge badge-danger">Inactive</span>
+                @endif
+            </td>
+            <td>
+                <a href="{{ route('customers.edit',$customer->id) }}" class="btn btn-sm btn-info">Edit</a>
+
+                <form action="{{ route('customers.destroy',$customer->id) }}" method="POST" style="display:inline;">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-sm btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+
+    </table>
+
 </div>
+
 @endsection
-
-@push('styles')
-<link rel="stylesheet" href="{{ asset('admin/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-<link rel="stylesheet" href="{{ asset('admin/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
-@endpush
-
-@push('scripts')
-<script src="{{ asset('admin/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('admin/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('admin/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-<script src="{{ asset('admin/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
-<script>
-$(function () {
-    $('#customers-table').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: "{{ route('customers.datatable') }}",
-        columns: [
-            { data: 'id', name: 'id' },
-            { data: 'name', name: 'name' },
-            { data: 'email', name: 'email' },
-            { data: 'mobile', name: 'mobile' },
-            { data: 'status', name: 'status' },
-            { data: 'created_at', name: 'created_at' },
-            { data: 'actions', name: 'actions', orderable: false, searchable: false }
-        ],
-        responsive: true,
-        autoWidth: false,
-        order: [[0, 'desc']],
-        language: {
-            paginate: {
-                next: '&gt;',
-                previous: '&lt;'
-            }
-        }
-    });
-});
-</script>
-@endpush
